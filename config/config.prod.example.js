2e66cdfa4bd71206411f729adfbd707448e4cae6
Liu Ji <mynameisliuji@foxmail.com>
2023-03-02 21:32:45 +0800

    config.prod

--- config/config.prod.example.js
+++ config/config.prod.example.js
@@ -26,7 +26,7 @@ module.exports = appInfo => {
         dialect: 'mysql',
         connection: {
           host: '127.0.0.1',
-          port: '3306',
+          port: 3306,
           user: 'root',
           password: '123456',
           database: 'zhiyuan_v6_hr'
